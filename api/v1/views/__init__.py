b0e4a7f7d89b0d39736826e4af3d04cc9da4c29b
ahmed_mustafa <ahmed_mustafa@mail.com>
2023-09-29 12:31:32 -0700

    task 3 update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Initialize the views blueprint"""
+'''
+Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
+'''
+
+
 from flask import Blueprint
 
-app_views = Blueprint("base", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -12,3 +16,4 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+
